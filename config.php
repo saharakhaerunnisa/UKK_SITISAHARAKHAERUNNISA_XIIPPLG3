c6b4e951010d3dedc75021d9bb15259125d6a731
Sahara <164311327+saharakhaerunnisa@users.noreply.github.com>
2026-04-14 18:04:38 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "db_uts";
+
+    $conn = mysqli_connect($server, $username, $password, $database);
+?>
+
